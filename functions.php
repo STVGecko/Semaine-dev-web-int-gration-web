0dc096d581c34a33e3ea5d991b55fbb6a13b5e30
STVGecko <steeven.lombardi@gmail.com>
2020-11-16 17:06:05 +0100

    Function définitif !!

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
-<?php function scripts() {
+<?php
+
+//ajout de css et de js dans le header
+function scripts() {
+
   wp_enqueue_style( 'reset', get_template_directory_uri().'/css/reset.css');
-  wp_enqueue_style( 'general', get_template_directory_uri().'/css/site.css');
   wp_enqueue_style( 'index', get_template_directory_uri().'/css/index.css');
   wp_enqueue_style( 'header', get_template_directory_uri().'/css/header.css');
   wp_enqueue_style( 'footer', get_template_directory_uri().'/css/footer.css');
   //wp_enqueue_script(); si besoin de js
 }
-?>
+add_action( 'wp_enqueue_scripts', 'scripts' );
