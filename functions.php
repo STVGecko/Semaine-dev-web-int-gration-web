2fdfab3160bbb111b4e075590e9dea047b19a9dd
STVGecko <steeven.lombardi@gmail.com>
2020-11-16 16:15:18 +0100

    functions2

--- functions.php
+++ functions.php
@@ -4,7 +4,8 @@
 
 
 function scripts() {
-  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_style( 'reset', get_template_directory_uri().'/css/reset.css');
   wp_enqueue_style( 'general', get_template_directory_uri().'/css/site.css');
+  //wp_enqueue_script(); si besoin de js
 }
 add_action( 'wp_enqueue_scripts', 'scripts' );
