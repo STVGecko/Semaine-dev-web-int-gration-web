a27bb33fdcc0513a5dfdc60642f26f0f50711139
STVGecko <steeven.lombardi@gmail.com>
2020-11-16 14:05:51 +0100

    header ajout image

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<meta name="viewport" content="width=device-width">
+	<meta charset="utf-8">
+</head>
+<body>
+  <header>
+            <div class="logo_menu">
+                <a href="#"><img src="images/Logotype.svg"></a>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">POLITICS</a></li>
+                    <li><a href="#">ENTERTAINMENT</a></li>
+                    <li><a href="#">LIFESTYLE</a></li>
+                </ul>
+            </div>
+            <div class="picto">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+  </header>
+</body>
+</html>
