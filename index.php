a618f869b4bcd47912e156d97a908b30ec9cc530
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-11-16 15:15:31 +0100

    modif commentaires

--- index.php
+++ index.php
@@ -18,32 +18,15 @@
 		</div>
 
 		<div id="wrap">
-<<<<<<< HEAD
 				<div class="icon" name="Rénovation"><img src="images/logo_renovation.png"></div>
 				<div class="icon" name="Terrassement"><img src="images/logo_terrassement.png"></div>
 				<div class="icon" name="Soubassement"><img src="images/logo_soubassement.png"></div>
 			</div>
 		<div id="wrap2">
-=======
 
 				<div class="icon"><img src="images/logo_renovation.png">test</div>
 				<div class="icon"><img src="images/logo_terrassement.png">test</div>
 				<div class="icon"><img src="images/logo_soubassement.png">test</div>
-<<<<<<< HEAD
-
-			<!--<div id="line2">
->>>>>>> 77dd8185b077f91b66bbd81e57e7f3e16829569b
-				<div class="icon_4" name="Dallage"><img src="images/logo_dallage.png"></div>
-				<div class="icon_5" name="Piscine"><img src="images/logo_piscine.png"></div>
-				<div class="icon_6" name="Terrasse"><img src="images/logo_terrasse.png"></div>
-		</div>
-=======
-			</div>
-		<div id="wrap2">
-				<div class="icon_4" name="Dallage"><img src="images/logo_dallage.png"></div>
-				<div class="icon_5" name="Piscine"><img src="images/logo_piscine.png"></div>
-				<div class="icon_6" name="Terrasse"><img src="images/logo_terrasse.png"></div>
->>>>>>> 823ae52e363624e9a2cc7a909683acde37e17906
 		</div>
 	</section>
 
