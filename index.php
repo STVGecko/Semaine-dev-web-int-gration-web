2ae62e9c95154ab35119ed6f409f1b9881509fdf
STVGecko <steeven.lombardi@gmail.com>
2020-11-16 13:11:54 +0100

    ceci est le html minimal de chaque page (on verra si on enlève ou non le header et footer comme il seront appelés)

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+
+	</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<meta name="viewport" content="width=device-width">
+	<meta charset="utf-8">
+</head>
+<body>
+	<header>
+
+	</header>
+	<footer>
+
+	</footer>
+</body>
+</html>
