96f09d11cfb33b8568b46354e3d03f76346856be
STVGecko <steeven.lombardi@gmail.com>
2020-11-16 16:38:09 +0100

    headr.php

--- header.php
+++ header.php
@@ -9,12 +9,15 @@
 <body>
   <header>
     <div class="barre_info">
+			<div class="logo">
+				<img src="<?php echo get_template_directory_uri();?>/images/logo.svg" alt="logo m2a">
+			</div>
       <div class="telephone">
-        <img src="<?php echo get_template_directory_uri();?>/images/tel_header.svg">
+        <img src="<?php echo get_template_directory_uri();?>/images/tel_header.svg" alt="téléphone"">
         <span>06 74 11 63 45</span>
       </div>
       <div class="mail">
-        <img src="<?php echo get_template_directory_uri();?>/images/mail_header.svg">
+        <img src="<?php echo get_template_directory_uri();?>/images/mail_header.svg" alt="téléphone">
         <span>pa.flamens.@orange.fr</span>
       </div>
       <div class="devis">
