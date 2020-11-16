141a92365fed782590fd20dec1567879750904e0
STVGecko <steeven.lombardi@gmail.com>
2020-11-16 18:36:14 +0100

    header.css 4

--- header.php
+++ header.php
@@ -8,29 +8,26 @@
 </head>
 <body>
   <header>
-		<div class="logo_menuglobal">
 			<div class="logo">
 				<img src="<?php echo get_template_directory_uri();?>/images/logo.svg" alt="logo m2a maçonnerie d'antan et d'aujoud'hui">
 			</div>
 			<div class="menuglobal">
-					<div class="barre_info">
-			      <div class="telephone">
-			        <img src="<?php echo get_template_directory_uri();?>/images/tel_header.svg">
-			        <div class="texte_icone">06 74 11 63 45</div>
+				<div class="barre_info">
+		      <div class="telephone">
+		        <img src="<?php echo get_template_directory_uri();?>/images/tel_header.svg">
+		        <div class="texte_icone">06 74 11 63 45</div>
+		      </div>
+		      <div class="mail">
+		        <img src="<?php echo get_template_directory_uri();?>/images/mail_header.svg">
+		        <divclass="texte_icone">pa.flamens.@orange.fr</div>
+						<div class="devis">
+			        <span>Demande de devis</span>
 			      </div>
-			      <div class="mail">
-			        <img src="<?php echo get_template_directory_uri();?>/images/mail_header.svg">
-			        <divclass="texte_icone">pa.flamens.@orange.fr</div>
-							<div class="devis">
-				        <span>Demande de devis</span>
-				      </div>
-			    </div>
+		    </div>
+		    <div id="menuprincipal">
+		      <?php wp_page_menu('show_home=1'); ?>
+		    </div>
 			</div>
-		</div>
-	    <div id="menuprincipal">
-	      <?php wp_page_menu('show_home=1'); ?>
-	    </div>
-</div>
   </header>
 </body>
 </html>
