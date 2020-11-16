c0bfb08aa38b0cafd1b0c8e46fb5c750e4ea4b6b
Priet0 <diegoprieto36@outlook.fr>
2020-11-16 17:18:25 +0100

    texte supp

--- index.php
+++ index.php
@@ -9,22 +9,29 @@
 				<?php ?>
 		</div>
 
+		<div id="texte_savoir-faire">
+
+			<div id="paragraphe">
+					<p> M2A a pour objectif de mettre à votre disposition son expérience, ses qualifications et
+						son savoir-faire afin de vous garantir des travaux de qualité qui répondent à vos besoins. </p>
+			</div>
+
 		<div id="logo_savoir-faire">
 
 				<div id="titre1"> <h3> Bâti ancien </h3> </div>
 
 				<div id="wrap1">
-					<div class="icon" name="Rénovation"><img src="images/logo_renovation.png" alt="Icône restauration"></div>
-					<div class="icon" name="Terrassement"><img src="images/logo_terrassement.png" alt="Icône terrassement"></div>
-					<div class="icon" name="Soubassement"><img src="images/logo_soubassement.png" alt="Icône reprise en sous-oeuvre"></div>
+					<div class="icon" name="Rénovation"><img src="<?php echo get_template_directory_uri();?>/images/logo_renovation.png" alt="Icône restauration"></div>
+					<div class="icon" name="Terrassement"><img src="<?php echo get_template_directory_uri();?>/images/logo_terrassement.png" alt="Icône terrassement"></div>
+					<div class="icon" name="Soubassement"><img src="<?php echo get_template_directory_uri();?>/images/logo_soubassement.png" alt="Icône reprise en sous-oeuvre"></div>
 				</div>
 
 				<div id="titre2"> <h3> Maçonnerie traditionnelle </h3> </div>
 
 				<div id="wrap2">
-					<div class="icon"><img src="images/logo_dallage.png" alt="Icône agrandissement"></div>
-					<div class="icon"><img src="images/logo_piscine.png" alt="Icône maçonnerie paysagère"></div>
-					<div class="icon"><img src="images/logo_terrasse.png" alt="Icône enduit"></div>
+					<div class="icon"><img src="<?php echo get_template_directory_uri();?>/images/logo_dallage.png" alt="Icône agrandissement"></div>
+					<div class="icon"><img src="<?php echo get_template_directory_uri();?>/images/logo_piscine.png" alt="Icône maçonnerie paysagère"></div>
+					<div class="icon"><img src="<?php echo get_template_directory_uri();?>/images/logo_terrasse.png" alt="Icône enduit"></div>
 				</div>
 
 			</div>
@@ -32,3 +39,6 @@
 	</section>
 
 <?php get_footer(); ?>
+
+
+"NOTRE SAVOIR-FAIRE" Ce sera une image ou du css ???
