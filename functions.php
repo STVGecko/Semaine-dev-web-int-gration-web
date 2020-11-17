ef84a62a29f08e303369f5d8615ecdab231f8ff6
AnthonyNhlg <antho.nehlig@gmail.com>
2020-11-17 15:30:00 +0100

    functions.php

--- functions.php
+++ functions.php
@@ -7,6 +7,9 @@ function scripts() {
   wp_enqueue_style( 'index', get_template_directory_uri().'/css/index.css');
   wp_enqueue_style( 'header', get_template_directory_uri().'/css/header.css');
   wp_enqueue_style( 'footer', get_template_directory_uri().'/css/footer.css');
+  wp_enqueue_style( 'page-savoirfaire', get_template_directory_uri().'/css/page-savoirfaire.css');
+  wp_enqueue_style( 'page-apropos', get_template_directory_uri().'/css/page-apropos.css');
+  wp_enqueue_style( 'page-photo', get_template_directory_uri().'/css/page-photo.css');
   wp_enqueue_style( 'page-contact', get_template_directory_uri().'/css/page-contact.css');
   //wp_enqueue_script(); si besoin de js
 }
