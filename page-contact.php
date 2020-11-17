6821190d0ec5b3aa754ee4b633b059b71e2405bf
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-11-17 14:07:18 +0100

    css page contact

--- page-contact.php
+++ page-contact.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <body>
+  <!-- Présentation de la page -->
 <section id="contact_page">
   <h2>Pour nous contacter</h2>
   <div id="underline"></div>
@@ -10,12 +11,41 @@
     <p id="small_text">Si vous avez la moindre question, ou si vous souhaitez nous demander un devis, n’hésitez pas.</p>
     <div class="test"></div>
   </div>
+  <!-- Partie image -->
   <div id="wrap_contact">
     <div class="icon_contact" name="Telephone">
+      <img src="<?php echo get_template_directory_uri();?>/images/Telephone.png" alt="Icône téléphone">
+      <div class="text_contact" name="Telephone">
+        <p>06 74 11 63 45</p>
+      </div>
     </div>
-    <div class="icon_contact" name="Mail"></div>
-    <div class="icon_contact" name="Position"></div>
+    <div class="icon_contact" name="Mail">
+      <img src="<?php echo get_template_directory_uri();?>/images/mail.png" alt="Icône mail">
+      <div class="text_contact" name="Mail">
+        <p>pa.flamens@orange.fr</p>
+      </div>
+    </div>
+    <div class="icon_contact" name="Position">
+      <img src="<?php echo get_template_directory_uri();?>/images/position.png" alt="Icône position">
+      <div class="text_contact" name="Position">
+        <p>68 Rue Bobby Sands<br>73000 Chambéry</p>
+        <p>Ouvrir dans Maps ></p>
+      </div>
+    </div>
   </div>
+    <!-- Partie texte -->
+  <!--<div id="text_wrap_contact">
+    <div class="text_contact" name="Telephone">
+      <p>06 74 11 63 45</p>
+    </div>
+    <div class="text_contact" name="Mail">
+      <p>pa.flamens@orange.fr</p>
+    </div>
+    <div class="text_contact" name="Position">
+      <p>68 Rue Bobby Sands<br>73000 Chambéry</p>
+      <p>Ouvrir dans Maps ></p>
+    </div>
+  </div>-->
 </section>
 <div id="devis_contact">
 
