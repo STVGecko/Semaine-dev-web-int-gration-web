b85384f80e82b336a63d9879aeeee5da84177be0
AnthonyNhlg <antho.nehlig@gmail.com>
2020-11-17 14:02:03 +0100

    base apropos.css

---
+++ apropos/page-apropos.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+
+<body>
+  <section id="apropos_page">
+    <h2>A propos</h2>
+    <div id="underline"></div>
+    <br>
+    <div id="presentation">
+      <p id="intro_text">L’entreprise qui vous aide à bâtir vos rêves. </p>
+    </div>
+    <img src="<?php echo get_template_directory_uri();?>/images/vehicule_m2a.png" alt="Véhicule M2A">
+    <div id="presentation_entreprise">
+      <p id="bold_text">M2A</p>
+      <p id="small_text">
+        Créée en 2010, M2A est une entreprise de maçonnerie spécialisée dans la restauration du patrimoine et la rénovation du bâti ancien.
+        Nous avons commencé par réaliser tout type de travaux de maçonnerie traditionnelle (reprises en sous-oeuvres, extensions, piscines, travaux paysagers, etc.) pour enfin nous rendre compte de l’importance du bâti ancien et nous spécialiser dans la sauvegarde de celui-ci à l’aide de matériaux sains et écologiques.
+        Basée au Bourget-du-Lac, nous intervenons principalement en Savoie, mais aussi sur l’ensemble de la région Auvergne-Rhône-Alpes.
+        Avec plus de 10 ans d’existence, M2A est une entreprise sérieuse, fiable, soucieuse de la qualité de ses prestations et réalisations.
+      </p>
+    </div>
+    <div id="presentation_flamens">
+      <p id="bold_text">Pierre-André FLAMENS</p>
+      <p id="metier_text">gérant de la société M2A</p>
+      <p id="small_text">
+        « Durant mes années de lycée j’ai découvert la maçonnerie. Après avoir obtenu un BAC Scientifique, je me suis orienté vers un CAP Maçonnerie à la FCMB (Fédération Compagnonnique des Métiers du Bâtiment) à Lyon en 2005, dont je suis sorti major de ma promotion. A la suite du CAP, j’ai intégré les Compagnons du Tour de France.
+        En 2008, j’ai obtenu un DUT Génie Civil. Ensuite, j’ai travaillé 2 années en tant que maçon, puis j’ai créé la société M2A. Afin de perfectionner mes compétences dans le bâti ancien, j’ai obtenu le DU Patrimoine et Gestion Durable à l’Université Lyon 2 en 2019. »
+      </p>
+      <img src="<?php echo get_template_directory_uri();?>/images/Pierre-Andre_Flamens.png" alt="Pierre-André Flamens">
+    </div>
+
+  </section>
+
+
+
+  </body>
+
+<?php get_footer(); ?>
