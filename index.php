c315dc84793d4c2c175b8c212f951c7159e1965c
Priet0 <diegoprieto36@outlook.fr>
2020-11-17 09:25:50 +0100

    css

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		<div id="texte_savoir-faire">
 
 			<div class="border"> <h2> Notre savoir-faire </h2> </div>
-			<div id="paragraphe">
+			<div class="paragraphe">
 					<p> M2A a pour objectif de mettre à votre disposition son expérience, ses qualifications et <br/>
 						son savoir-faire afin de vous garantir des travaux de qualité qui répondent à vos besoins. </p>
 			</div>
@@ -50,7 +50,7 @@
 
 		<div class="border">	<h2> À-propos </h2> </div>
 
-			<div id="paragraphe">
+			<div class="paragraphe">
 
 				<p> Basée au Bourget du Lac, nous intervenons principalement en <br/>
 					Savoie, mais aussi sur l'ensemble de la région <br/>
