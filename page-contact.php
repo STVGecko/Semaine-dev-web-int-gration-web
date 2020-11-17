1b767b676c1304af489d6ceb578d923ea42b960c
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-11-17 10:50:42 +0100

    css contact

--- page-contact.php
+++ page-contact.php
@@ -4,9 +4,17 @@
 <section id="contact_page">
   <h2>Pour nous contacter</h2>
   <div id="presentation">
-    <p>Pierre-André FLAMENS, gérant de la société M2A</p>
+    <p id="bold_text">Pierre-André FLAMENS, gérant de la société M2A</p>
     <p>Si vous avez la moindre question, ou si vous souhaitez nous demander un devis, n’hésitez pas.</p>
     <div class="test"></div>
+  </div>
+  <div id="wrap">
+    <div class="icon" name="Telephone"><div class="image"<img src="<?php echo get_template_directory_uri();?>/images/Telephone.svg" alt="Icône téléphone"></div>
+    </div>
+    <div class="icon" name="Mail"><div class="image"<img src="<?php echo get_template_directory_uri();?>/images/mail.svg" alt="Icône mail"></div>
+    </div>
+    <div class="icon" name="Position"><div class="image"<img src="<?php echo get_template_directory_uri();?>/images/position" alt="Icône position"></div>
+    </div>
   </div>
 
 
