6b1ab0603866ebc21cd81b8d5dd2f78a20c07a26
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-11-17 09:54:43 +0100

    paragraphe

--- page-contact.php
+++ page-contact.php
@@ -1,8 +1,13 @@
 <?php get_header(); ?>
 
 <body>
+<section id="contact_page">
   <h2>Pour nous contacter</h2>
-  
+  <div id="presentation">
+    <p>Pierre-André FLAMENS, gérant de la société M2A</p>
+    <p>Si vous avez la moindre question, ou si vous souhaitez nous demander un devis, n’hésitez pas.</p>
+  </div>
+
 
 
 
