d86b9323a838f90978aef61783a4d1d2a1e6d519
AnthonyNhlg <antho.nehlig@gmail.com>
2020-11-17 15:21:54 +0100

    page photo.html et css

--- page-photo.php
+++ page-photo.php
@@ -6,20 +6,46 @@
     <div id="underline"></div>
     <br>
     <div id="galerie_photos">
-      <img src="<?php echo get_template_directory_uri();?>/images/piscine/piscine_1.png" alt="Construction d'une piscine">
-      <img src="<?php echo get_template_directory_uri();?>/images/piscine/piscine_2.png" alt="Construction d'une piscine">
-      <img src="<?php echo get_template_directory_uri();?>/images/piscine/piscine_3.png" alt="Construction d'une piscine">
-      <img src="<?php echo get_template_directory_uri();?>/images/piscine/piscine_4.png" alt="Construction d'une piscine">
-      <img src="<?php echo get_template_directory_uri();?>/images/piscine/piscine_5.png" alt="Construction d'une piscine">
+
+      <div id="piscine_1">
+        <img src="<?php echo get_template_directory_uri();?>/images/piscine/piscine_1.png" alt="Construction d'une piscine">
+      </div>
+
+      <div id="piscine2">
+        <img src="<?php echo get_template_directory_uri();?>/images/piscine/piscine_2.png" alt="Construction d'une piscine">
+      </div>
+
+      <div id="piscine3">
+        <img src="<?php echo get_template_directory_uri();?>/images/piscine/piscine_3.png" alt="Construction d'une piscine">
+      </div>
+
+      <div id="piscine4">
+        <img src="<?php echo get_template_directory_uri();?>/images/piscine/piscine_4.png" alt="Construction d'une piscine">
+      </div>
+
+      <div id="piscine5">
+        <img src="<?php echo get_template_directory_uri();?>/images/piscine/piscine_5.png" alt="Construction d'une piscine">
+      </div>
+
     </div>
 
     <h2>Mur de soutennement</h2>
     <div id="underline"></div>
     <br>
     <div id="galerie_photos">
-      <img src="<?php echo get_template_directory_uri();?>/images/mur_soutennement/mur_soutennement_1.png" alt="Construction d'un mur de soutennement">
-      <img src="<?php echo get_template_directory_uri();?>/images/mur_soutennement/mur_soutennement_2.png" alt="Construction d'un mur de soutennement">
-      <img src="<?php echo get_template_directory_uri();?>/images/mur_soutennement/mur_soutennement_3.png" alt="Construction d'un mur de soutennement">
+
+      <div id="mur1">
+        <img src="<?php echo get_template_directory_uri();?>/images/mur_soutennement/mur_soutennement_1.png" alt="Construction d'un mur de soutennement">
+      </div>
+
+      <div id="mur2">
+        <img src="<?php echo get_template_directory_uri();?>/images/mur_soutennement/mur_soutennement_2.png" alt="Construction d'un mur de soutennement">
+      </div>
+
+      <div id="mur3">
+        <img src="<?php echo get_template_directory_uri();?>/images/mur_soutennement/mur_soutennement_3.png" alt="Construction d'un mur de soutennement">
+      </div>
+
     </div>
   </section>
 
