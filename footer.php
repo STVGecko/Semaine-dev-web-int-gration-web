beadba12123616833853bd7de03302a56625013f
STVGecko <steeven.lombardi@gmail.com>
2020-11-17 14:08:18 +0100

    footer.php

--- footer.php
+++ footer.php
@@ -7,32 +7,25 @@
       </div>
 			<div class="touttext_footer">
 	      <div class="text_logo_footer">
-	        <ul class="list-text_logo_footer">
-	          <li class="text_logo_footer_t1">Maçonnerie</li>
-	          <li class="text_logo_footer_t2">d’antan et</li>
-	          <li class="text_logo_footer_t3">d’aujourd’hui</li>
-	        </ul>
+	          <p>Maçonnerie</p>
+	          <p>d’antan et</p>
+	          <p>d’aujourd’hui</p>
 	      </div>
 				<div class="coordonnees">
-					<div class="title_footer">Coordonnées</div>
-					<ul class="list-coordonees_footer">
-						<li class="coordonnees_t1">06 74 11 63 45</li>
-						<li class="coordonnees_t2">pa.flamens@orange.fr</li>
-					</ul>
+					<p>Coordonnées</p>
+						<p>06 74 11 63 45</p>
+						<p>pa.flamens@orange.fr</p>
 				</div>
 				<div class="adresse">
-					<ul class="list-adresse_footer">
-						<li class="adresse_t1">68 Rue Bobby Sands</li>
-						<li class="adresse_t2">73000 Chambéry</li>
-					</ul>
+						<p>68 Rue Bobby Sands</p>
+						<p>73000 Chambéry</p>
 				</div>
 	      <div class="rs">
-					<div class="title_footer">Réseaux sociaux</div>
-					<ul class="list-rs_footer">
-						<li><a href=""><img src="<?php echo get_template_directory_uri();?>/images/logo_facebook_footer.svg" alt="Logo Facebook" /></a></li>
-						<li class="facebook"><a href="">Facebook</a></li>
-					</ul>
-				</div>
+					<p>Réseaux sociaux</p>
+					<div class="facebook">
+						<a href=""><img src="<?php echo get_template_directory_uri();?>/images/logo_facebook_footer.svg" alt="Logo Facebook" /></a></li>
+						<a href="">Facebook</a>
+					</div>
 	      <div class="copyright">
 					<ul class="list-copyright_footer">
 						<li class="copyright_t1"><a href="">Données personnelles</a></li>
