08ad8f644c51ba65193032dbfef92d4abc51040a
AnthonyNhlg <antho.nehlig@gmail.com>
2020-11-17 14:36:39 +0100

    base page-phot

---
+++ page-photo.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<body>
+  <section id="photos_page">
+    <h2>Projet construction piscine</h2>
+    <div id="underline"></div>
+    <br>
+
+    <h2>Mur de soutennement</h2>
+    <div id="underline"></div>
+    <br>
+  </section>
+
+</body>
+
+  <?php get_footer(); ?>
