456f4ef07cd83a64ba7293e430a91061b54ec37f
AnthonyNhlg <antho.nehlig@gmail.com>
2020-11-17 13:39:19 +0100

    page-savoirfaire.php

---
+++ savoirfaire/page-savoirfaire.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<body>
+  <section id="savoirfaire_page">
+    <h2>Savoir-faire</h2>
+    <div id="underline"></div>
+    <br>
+    <div id="presentation">
+      <p id="intro_text">M2A a pour objectif de mettre à votre disposition son expérience, ses qualifications et son savoir faire afin de vous garantir des travaux de qualité qui répondent à vos besoins.</p>
+    </div>
+  </section>
+
+
+
+  </body>
+
+<?php get_footer(); ?>
