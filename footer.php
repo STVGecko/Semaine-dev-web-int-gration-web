7f1ca520bc2076ea5344dbb6a71f9e9f231e77cf
STVGecko <steeven.lombardi@gmail.com>
2020-11-17 13:56:05 +0100

    header et footer

--- footer.php
+++ footer.php
@@ -1,49 +1,52 @@
 <footer>
-	<section id="container">
+	<div class="wrap_footer">
       <div class="logo_footer">
         <ul class="list-logo_footer">
-          <li><a href="#"><img class="logo" src="<?php echo get_template_directory_uri();?>/images/logo_sanstxt.svg" alt="M2A Maçonnerie" /></a></li>
+          <a href="#"><img class="logo" src="<?php echo get_template_directory_uri();?>/images/logo_sanstxt.svg" alt="M2A Maçonnerie" /></a>
         </ul>
       </div>
-      <div class="text_logo_footer">
-        <ul class="list-text_logo_footer">
-          <li class="text_logo_footer_t1">Maçonnerie</li>
-          <li class="text_logo_footer_t2">d’antan et</li>
-          <li class="text_logo_footer_t3">d’aujourd’hui</li>
-        </ul>
-      </div>
-			<div class="coordonnees">
-				<div class="title_footer">Coordonnées</div>
-				<ul class="list-coordonees_footer">
-					<li class="coordonnees_t1">06 74 11 63 45</li>
-					<li class="coordonnees_t2">pa.flamens@orange.fr</li>
-				</ul>
-			</div>
-			<div class="adresse">
-				<ul class="list-adresse_footer">
-					<li class="adresse_t1">68 Rue Bobby Sands</li>
-					<li class="adresse_t2">73000 Chambéry</li>
-				</ul>
-			</div>
-      <div class="rs">
-				<div class="title_footer">Réseaux sociaux</div>
-				<ul class="list-rs_footer">
-					<li><a href=""><img class="logo_fb" src="<?php echo get_template_directory_uri();?>/images/logo_facebook_footer.svg" alt="Logo Facebook" /></a></li>
-					<li class="facebook"><a href="">Facebook</a></li>
-				</ul>
-			</div>
-      <div class="copyright">
-				<ul class="list-copyright_footer">
-					<li class="copyright_t1"><a href="">Données personnelles</a></li>
-          <li class="copyright_t2"><a href="">Mentions légales</a></li>
-          <li class="copyright_t3">© 2020, Kanzaï️</li>
-				</ul>
-			</div>
-	</section>
-	<div class="background_footer">
-		<p>M2A</p>
-		<hr>
-	</div>
+			<div class="touttext_footer">
+	      <div class="text_logo_footer">
+	        <ul class="list-text_logo_footer">
+	          <li class="text_logo_footer_t1">Maçonnerie</li>
+	          <li class="text_logo_footer_t2">d’antan et</li>
+	          <li class="text_logo_footer_t3">d’aujourd’hui</li>
+	        </ul>
+	      </div>
+				<div class="coordonnees">
+					<div class="title_footer">Coordonnées</div>
+					<ul class="list-coordonees_footer">
+						<li class="coordonnees_t1">06 74 11 63 45</li>
+						<li class="coordonnees_t2">pa.flamens@orange.fr</li>
+					</ul>
+				</div>
+				<div class="adresse">
+					<ul class="list-adresse_footer">
+						<li class="adresse_t1">68 Rue Bobby Sands</li>
+						<li class="adresse_t2">73000 Chambéry</li>
+					</ul>
+				</div>
+	      <div class="rs">
+					<div class="title_footer">Réseaux sociaux</div>
+					<ul class="list-rs_footer">
+						<li><a href=""><img src="<?php echo get_template_directory_uri();?>/images/logo_facebook_footer.svg" alt="Logo Facebook" /></a></li>
+						<li class="facebook"><a href="">Facebook</a></li>
+					</ul>
+				</div>
+	      <div class="copyright">
+					<ul class="list-copyright_footer">
+						<li class="copyright_t1"><a href="">Données personnelles</a></li>
+	          <li class="copyright_t2"><a href="">Mentions légales</a></li>
+	          <li class="copyright_t3">© 2020, Kanzaï️</li>
+					</ul>
+				</div>
+		</section>
+		<!--<div class="background_footer">
+			<p>M2A</p>
+			<hr>
+		</div>-->
+		</div>
+</div>
 <?php wp_footer(); ?>
 </footer>
 
