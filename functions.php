bc48d4d79d690dd6bbef2b8ab0514ad6ec4f7fd2
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-11-17 10:37:00 +0100

    functions ajout contact

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ function scripts() {
   wp_enqueue_style( 'index', get_template_directory_uri().'/css/index.css');
   wp_enqueue_style( 'header', get_template_directory_uri().'/css/header.css');
   wp_enqueue_style( 'footer', get_template_directory_uri().'/css/footer.css');
+  wp_enqueue_style( 'page-contact', get_template_directory_uri().'/css/page-contact.css');
   //wp_enqueue_script(); si besoin de js
 }
 add_action( 'wp_enqueue_scripts', 'scripts' );
