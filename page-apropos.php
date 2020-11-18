3ef627dff202328fad2682158cabbee0210575e5
Priet0 <diegoprieto36@outlook.fr>
2020-11-18 11:44:29 +0100

    restructuration texte apropos.php

--- page-apropos.php
+++ page-apropos.php
@@ -12,19 +12,42 @@
     <div id="presentation_entreprise">
       <p id="bold_text">M2A</p>
       <p id="small_text">
-        Créée en 2010, M2A est une entreprise de maçonnerie spécialisée dans la restauration du patrimoine et la rénovation du bâti ancien.
-        Nous avons commencé par réaliser tout type de travaux de maçonnerie traditionnelle (reprises en sous-oeuvres, extensions, piscines, travaux paysagers, etc.) pour enfin nous rendre compte de l’importance du bâti ancien et nous spécialiser dans la sauvegarde de celui-ci à l’aide de matériaux sains et écologiques.
-        Basée au Bourget-du-Lac, nous intervenons principalement en Savoie, mais aussi sur l’ensemble de la région Auvergne-Rhône-Alpes.
-        Avec plus de 10 ans d’existence, M2A est une entreprise sérieuse, fiable, soucieuse de la qualité de ses prestations et réalisations.
-      </p>
+        Créée en 2010, M2A est une entreprise de maçonnerie<br/>
+        spécialisée dans la restauration du patrimoine et la<br/>
+        rénovation du bâti ancien.<br/>
+        <br/>
+        Nous avons commencé par réaliser tout type de<br/>
+        travaux de maçonnerie traditionnelle (reprises en<br/>
+        sous-oeuvres, extensions, piscines, travaux paysagers,<br/>
+        etc.) pour enfin nous rendre compte de l’importance du<br/>
+        bâti ancien et nous spécialiser dans la sauvegarde de<br/>
+        celui-ci à l’aide de matériaux sains et écologiques.<br/>
+        <br/>
+        Basée au Bourget-du-Lac, nous intervenons<br/>
+        principalement en Savoie, mais aussi sur l’ensemble de<br/>
+        la région Auvergne-Rhône-Alpes.<br/>
+        <br/>
+        Avec plus de 10 ans d’existence, M2A est une entreprise<br/>
+        sérieuse, fiable, soucieuse de la qualité de ses<br/>
+        prestations et réalisations. </p>
     </div>
     <div id="presentation_flamens">
       <p id="bold_text">Pierre-André FLAMENS</p>
       <p id="metier_text">gérant de la société M2A</p>
       <p id="small_text">
-        « Durant mes années de lycée j’ai découvert la maçonnerie. Après avoir obtenu un BAC Scientifique, je me suis orienté vers un CAP Maçonnerie à la FCMB (Fédération Compagnonnique des Métiers du Bâtiment) à Lyon en 2005, dont je suis sorti major de ma promotion. A la suite du CAP, j’ai intégré les Compagnons du Tour de France.
-        En 2008, j’ai obtenu un DUT Génie Civil. Ensuite, j’ai travaillé 2 années en tant que maçon, puis j’ai créé la société M2A. Afin de perfectionner mes compétences dans le bâti ancien, j’ai obtenu le DU Patrimoine et Gestion Durable à l’Université Lyon 2 en 2019. »
-      </p>
+        « Durant mes années de lycée j’ai découvert la<br/>
+        maçonnerie. Après avoir obtenu un BAC Scientifique, je<br/>
+        me suis orienté vers un CAP Maçonnerie à la FCMB<br/>
+        (Fédération Compagnonnique des Métiers du<br/>
+        Bâtiment) à Lyon en 2005, dont je suis sorti major de<br/>
+        ma promotion. A la suite du CAP, j’ai intégré les<br/>
+        Compagnons du Tour de France.<br/>
+        <br/>
+        En 2008, j’ai obtenu un DUT Génie Civil. Ensuite, j’ai<br/>
+        travaillé 2 années en tant que maçon, puis j’ai créé la<br/>
+        société M2A. Afin de perfectionner mes compétences<br/>
+        dans le bâti ancien, j’ai obtenu le DU Patrimoine et<br/>
+        Gestion Durable à l’Université Lyon 2 en 2019. » </p>
       <img src="<?php echo get_template_directory_uri();?>/images/Pierre-Andre_Flamens.png" alt="Pierre-André Flamens">
     </div>
 
