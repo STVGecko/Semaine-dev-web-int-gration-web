97f206ddcac55f8489e567ea251c293c8f2e7a1a
STVGecko <steeven.lombardi@gmail.com>
2020-11-18 15:18:55 +0100

    css+php3

--- page-savoirfaire.php
+++ page-savoirfaire.php
@@ -46,7 +46,7 @@
       <img src="<?php echo get_template_directory_uri();?>/images/cadre_gauche.svg" class="absolute">
       <div id="image1">
       </div>
-      <div id="texte">
+      <div class="texte">
         <h2 class="h2_flex"><?php $titre1 = get_field('titre1'); echo $titre1; ?></h2>
         <h3 class="h3_flex"><?php $stitre1 = get_field('stitre1'); echo $stitre1; ?></h3> <!--sous-titre ex : gérant de la maçonnerie-->
         <p class="para"><?php $texte1 = get_field('text1'); echo $texte1; ?></p>
@@ -59,7 +59,7 @@
   <img src="<?php echo get_template_directory_uri();?>/images/cadre_gauche.svg" class="absolute">
   <div id="image1">
   </div>
-  <div id="texte">
+  <div class="texte">
     <h2 class="h2_flex"><?php $titre2 = get_field('titre2'); echo $titre2; ?></h2>
     <h3 class="h3_flex"><?php $stitre2 = get_field('stitre2'); echo $stitre2; ?></h3> <!--sous-titre ex : gérant de la maçonnerie-->
     <p class="para"><?php $texte2 = get_field('text2'); echo $texte2; ?></p>
