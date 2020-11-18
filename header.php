2ccb668c5a9be1ba8ac4b0f2c7bed3befc300396
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-11-18 09:35:59 +0100

    bootstrap link

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
 	<meta http-equiv="cache-control" content="no-cache"/>
 	<meta charset="utf-8">
 	<script src="jquery-3.5.1.min.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 	<?php wp_head(); ?>
 </head>
 <body>
