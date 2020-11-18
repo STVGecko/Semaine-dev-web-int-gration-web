76b90359ab2b162a16b93467e86f86c2d8f8185c
STVGecko <steeven.lombardi@gmail.com>
2020-11-18 15:36:06 +0100

    php

--- page-savoirfaire.php
+++ page-savoirfaire.php
@@ -12,13 +12,46 @@
   <!-- Flickity HTML init -->
   <div class="gallery js-flickity"
     data-flickity-options='{ "wrapAround": true }'>
-    <div class="gallery-cell"><img src="images/carousel_1.png"</div>
+    <div class="gallery-cell"><img src="images/carousel_1.png"></div>
     <div class="gallery-cell"></div>
     <div class="gallery-cell"></div>
   </div>
 
 <section class="wrap">
+  <div class="bati_ancien">
+<div class="carrousel_gauche">
+      <img src="<?php $image1 = get_field('image1'); echo $image1; ?>"  alt="carroussel"> <!--carroussel-->
+      <ol class="vue_carousel">
+        <li id="photo1_carousel" tabindex="0" class="carousel_photo">
+          <div class="carousel_englobe">
+            <a href="#photo3_carousel" class="precedent_carousel">Aller à la photo précédente</a>
+            <a href="#photo2_carousel" class="suivant_carousel">Aller à la photo suivante</a>
+          </div>
+        </li>
+        <li id="photo2_carousel" tabindex="0" class="carousel_photo">
+          <div class="carousel_englobe">
+            <a href="#photo1_carousel" class="precedent_carousel">Aller à la photo précédente</a>
+            <a href="#photo3_carousel" class="suivant_carousel">Aller à la photo suivante</a>
+          </div>
+        </li>
+        <li id="photo3_carousel" tabindex="0" class="carousel_photo">
+          <div class="carousel_englobe">
+            <a href="#photo2_carousel" class="precedent_carousel">Aller à la photo précédente</a>
+            <a href="#photo1_carousel" class="suivant_carousel">Aller à la photo suivante</a>
+          </div>
+        </li>
+      </ol>
 
+    </div>
+    <img src="<?php echo get_template_directory_uri();?>/images/cadre_gauche.svg" class="absolute">
+    <div id="image1">
+    </div>
+    <div class="texte">
+      <h2 class="h2_flex"><?php $titre1 = get_field('titre1'); echo $titre1; ?></h2>
+      <h3 class="h3_flex"><?php $stitre1 = get_field('stitre1'); echo $stitre1; ?></h3> <!--sous-titre ex : gérant de la maçonnerie-->
+      <p class="para"><?php $texte1 = get_field('text1'); echo $texte1; ?></p>
+  </div>
+</div>
 <div class="maçonnerie">
   <div class="carrousel_droit">
     <img src="<?php $image2 = get_field('image2'); echo $image2; ?>"  alt="carroussel"> <!--carroussel-->
