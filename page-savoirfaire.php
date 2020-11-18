33fa014195ff093e29c753a380558aa23d36841c
STVGecko <steeven.lombardi@gmail.com>
2020-11-18 16:20:34 +0100

    css + php

--- page-savoirfaire.php
+++ page-savoirfaire.php
@@ -59,29 +59,6 @@
 
   </body>
 
-  <script type="text/javascript">
-  // external js: flickity.pkgd.js
 
-var $carousel = $('.carousel').flickity({
-  imagesLoaded: true,
-  percentPosition: false,
-});
-
-var $imgs = $carousel.find('.carousel-cell img');
-// get transform property
-var docStyle = document.documentElement.style;
-var transformProp = typeof docStyle.transform == 'string' ?
-  'transform' : 'WebkitTransform';
-// get Flickity instance
-var flkty = $carousel.data('flickity');
-
-$carousel.on( 'scroll.flickity', function() {
-  flkty.slides.forEach( function( slide, i ) {
-    var img = $imgs[i];
-    var x = ( slide.target + flkty.x ) * -1/3;
-    img.style[ transformProp ] = 'translateX(' + x  + 'px)';
-  });
-});
-</script>
 
 <?php get_footer(); ?>
