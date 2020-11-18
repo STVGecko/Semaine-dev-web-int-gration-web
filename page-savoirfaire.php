b1ededa161f1300e1bd959cc63631a3bc592a5ac
STVGecko <steeven.lombardi@gmail.com>
2020-11-18 13:20:18 +0100

    savoir-faire.php

--- page-savoirfaire.php
+++ page-savoirfaire.php
@@ -5,14 +5,30 @@
 
 
 
-
+<section class="wrap">
     <div class="bati_ancien">
+      <div class="carroussel_gauche">
+        <ol class="ca"
+
+
+      </div>
+      <h1>Bâti ancien</h1>
+      <p>Nous rénovons vos bâti ancien et effectuons divers aménagements : le drainage des pieds de mur,
+la reprise en sous œuvre et consolidation du bâti, l’enduits naturels de chaux, de terre, de chaux/chanvre, badigeons, le dallage ventilé, planchers collaborant ou l’extension brique ou pierre avec encadrement d’ouverture en pierre ou en bois.
+</p>
       <img src="<?php echo get_template_directory_uri();?>/images/cadre_gauche.svg">
     </div>
     <div class="maconnerie">
+      <div class="carroussel_droite">
+
+      </div>
+      <h1>Maçonnerie traditionnelle</h1>
+      <p>Nous pratiquons  la maçonnerie traditionelle où sont compris : la reprise en sous œuvre, création d’ouvertures, la rénovation et extension de maison, la maçonnerie paysagère, la rénovation et aménagement de piscines, bassins, terrasses ou encore le génie civil.
+</p>
+
       <img src="<?php echo get_template_directory_uri();?>/images/cadre_droit.svg">
     </div>
-  </section>
+</section>
 
 
 
