8f3accda2f9d95ec581251561e7f256e2d6a7800
STVGecko <steeven.lombardi@gmail.com>
2020-11-18 17:08:23 +0100

    php

--- page-savoirfaire.php
+++ page-savoirfaire.php
@@ -13,12 +13,7 @@
     data-flickity='{ "lazyLoad": true }'>
     <div class="carousel-cell">
     <img class="carousel-cell-image"
-      data-flickity-lazyload="
-      <?php
-      $image1 = get_field('image1_sf');
-      if( !empty( $image1 ) ): ?>
-          <img src="<?php echo esc_url($image1['url']); ?>" alt="<?php echo esc_attr($image1['alt']); ?>" />
-      <?php endif; ?>"/>
+      data-flickity-lazyload="<?php echo get_template_directory_uri();?>/images/carousel_1.png" alt="Photo1" />
     </div>
     <div class="carousel-cell">
     <img class="carousel-cell-image"
