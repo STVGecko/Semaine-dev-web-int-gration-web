53e9f28d305581da75b218a2c04bc79037fcc2d0
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-11-18 11:48:52 +0100

    test

--- page-savoirfaire.php
+++ page-savoirfaire.php
@@ -1,13 +1,11 @@
 <?php get_header(); ?>
 
 <body>
-  <section id="savoirfaire_page">
-    <h2 id="savoirfaire_title" >Savoir-faire</h2>
-    <div id="underline"></div>
-    <br>
-    <div id="presentation">
-      <p id="intro_text">M2A a pour objectif de mettre à votre disposition son expérience, ses qualifications et son savoir faire afin de vous garantir des travaux de qualité qui répondent à vos besoins.</p>
-    </div>
+
+
+
+
+
     <div class="bati_ancien">
       <img src="<?php echo get_template_directory_uri();?>/images/cadre_gauche.svg">
     </div>
