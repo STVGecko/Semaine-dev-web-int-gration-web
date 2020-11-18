bf320db8f8fa1c1e59fa5b0cdd8b1254d2e0627f
STVGecko <steeven.lombardi@gmail.com>
2020-11-18 15:10:00 +0100

    css php2

--- page-savoirfaire.php
+++ page-savoirfaire.php
@@ -62,7 +62,7 @@
   <div id="text1">
     <h2 class="h2_flex"><?php $titre2 = get_field('titre2'); echo $titre2; ?></h2>
     <h3 class="h3_flex"><?php $stitre2 = get_field('stitre2'); echo $stitre2; ?></h3> <!--sous-titre ex : gérant de la maçonnerie-->
-    <p class="para"><?php $texte1 = get_field('text2'); echo $texte2; ?></p>
+    <p class="para"><?php $texte2 = get_field('text2'); echo $texte2; ?></p>
 </div>
 </div>
 </section>
