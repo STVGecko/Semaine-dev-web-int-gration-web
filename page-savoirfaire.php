43afe5d47a7c9aa792a91a2ade53b9213212ff9a
STVGecko <steeven.lombardi@gmail.com>
2020-11-18 15:45:46 +0100

    savoir-faire.php

--- page-savoirfaire.php
+++ page-savoirfaire.php
@@ -49,14 +49,14 @@
       <h2 class="h2_flex"><?php $titre1 = get_field('titre1'); echo $titre1; ?></h2>
       <h3 class="h3_flex"><?php $stitre1 = get_field('stitre1'); echo $stitre1; ?></h3> <!--sous-titre ex : gérant de la maçonnerie-->
       <p class="para"><?php $texte1 = get_field('text1'); echo $texte1; ?></p>
-          <img src="<?php echo get_template_directory_uri();?>/images/cadre_gauche.svg" class="absolute">
+          <img src="<?php echo get_template_directory_uri();?>/images/cadre_gauche.svg">
   </div>
 </div>
 <div class="maçonnerie">
   <div class="carrousel_droit">
     <img src="<?php $image2 = get_field('image2'); echo $image2; ?>"  alt="carroussel"> <!--carroussel-->
   </div>
-  <img src="<?php echo get_template_directory_uri();?>/images/cadre_droit.svg" class="absolute">
+  <img src="<?php echo get_template_directory_uri();?>/images/cadre_droit.svg">
   <div id="image1">
   </div>
   <div class="texte">
