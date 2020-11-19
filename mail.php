fe8f1d1bdc4539f6111626b558680cad11750fef
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-11-19 11:41:46 +0100

    mail.php modifs

--- mail.php
+++ mail.php
@@ -1,1 +1,25 @@
+<?php
 
+
+$msg = "Nom:\t$prenom\n";
+$msg = "Nom:\t$nom\n";
+$msg .= "E-Mail:\t$mail\n";
+$msg = "Nom:\t$telephone\n";
+$msg .= "Message:\t$message\n\n";
+
+$recipient = "lucas.reymonet@gmail.com";
+$subject = "Formulaire";
+
+$mailheaders = "From: Formulaire<> \n";
+$mailheaders .= "Reply-To: $mail\n\n";
+
+mail($recipient, $subject, $msg, $nom, $mailheaders);
+
+echo "<HTML><HEAD>";
+echo "<TITLE>Formulaire validé</TITLE></HEAD><BODY>";
+echo "<H1 align=center>Merci, $nom </H1>";
+echo "<P align=center>";
+echo "Votre formulaire à bien été envoyé !</P>";
+echo "</BODY></HTML>";
+
+?>
