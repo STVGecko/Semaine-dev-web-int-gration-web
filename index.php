a75aaf980fd64d23bd8fc1240c1ec5c3bca39338
Priet0 <diegoprieto36@outlook.fr>
2020-11-19 17:11:30 +0100

    changemet lien admin home php fdp

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 	</section>
 
 		<div id="text_savoir-faire">
-			<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27">
+			<a href="<?php $savoir = get_field('lsavoir'); echo $savoir; ?>">
 				<div id="border1"> <h2> Notre savoir-faire </h2> </div>
 			</a>
 			<div id="paragraph1">
@@ -34,19 +34,19 @@
 		<div id="logo_savoir-faire">
 				<h3>Bâti ancien</h3>
 				<div id="wrap1">
-					<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27">
+					<a href="<?php $savoir = get_field('lsavoir'); echo $savoir; ?>">
 						<div class="icon" name="Restauration"><div class="picture1">
 							<img src="<?php echo get_template_directory_uri();?>/images/logo_restauration.svg" alt="Icône restauration"></div>
 							<div class="icon_name">Restauration</div>
 						</div>
 					</a>
-					<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27">
+					<a href="<?php $savoir = get_field('lsavoir'); echo $savoir; ?>">
 						<div class="icon" name="Maçonnerie"><div class="picture2">
 							<img src="<?php echo get_template_directory_uri();?>/images/logo_maconnerie.svg" alt="Icône terrassement"></div>
 							<div class="icon_name">Maçonnerie</div>
 						</div>
 					</a>
-					<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27">
+					<a href="<?php $savoir = get_field('lsavoir'); echo $savoir; ?>">
 						<div class="icon" name="Reprise en sous oeuvre"><div class="picture3">
 							<img src="<?php echo get_template_directory_uri();?>/images/logo_reprise.svg" alt="Icône reprise en sous-oeuvre"></div>
 							<div class="icon_name">Reprise en sous oeuvre</div>
@@ -55,19 +55,19 @@
 				</div>
 				<h3>Maçonnerie traditionnelle</h3>
 				<div id="wrap2">
-					<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27">
+					<a href="<?php $savoir = get_field('lsavoir'); echo $savoir; ?>">
 						<div class="icon" name="Agrandissement"><div class="picture4">
 							<img src="<?php echo get_template_directory_uri();?>/images/logo_agrandissement.svg" alt="Icône agrandissement"></div>
 							<div class="icon_name">Agrandissement</div>
 						</div>
 					</a>
-					<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27">
+					<a href="<?php $savoir = get_field('lsavoir'); echo $savoir; ?>">
 						<div class="icon" name="Maçonnerie paysagère"><div class="picture5">
 							<img src="<?php echo get_template_directory_uri();?>/images/logo_paysagere.svg" alt="Icône maçonnerie paysagère"></div>
 							<div class="icon_name">Maçonnerie paysagère</div>
 						</div>
 					</a>
-					<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27">
+					<a href="<?php $savoir = get_field('lsavoir'); echo $savoir; ?>">
 						<div class="icon" name="Enduit"><div class="picture6">
 							<img src="<?php echo get_template_directory_uri();?>/images/logo_enduit.svg" alt="Icône enduit"></div>
 							<div class="icon_name">Enduit</div>
@@ -91,7 +91,7 @@
 			</div>
 
 			<div id="a-propos_text">
-					<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=30">
+					<a href="<?php $propos = get_field('lpropos'); echo $propos; ?>">
 						<div id="border2">	<h2> À-propos </h2> </div>
 					</a>
 
@@ -99,7 +99,7 @@
 								<p> <?php $ap = get_field('ap_texte'); echo $ap; ?> </p>
 						</div>
 
-						<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=30"><p id="more">En savoir plus</p></a>
+						<a href="<?php $propos = get_field('lpropos'); echo $propos; ?>"><p id="more">En savoir plus</p></a>
 
 						<div class="absolute"><img src="<?php echo get_template_directory_uri();?>/images/arrow.svg" alt="Aller voir"></div>
 
@@ -112,7 +112,7 @@
 			<h3> Besoin de nos services ?
 				<br/> Contactez-nous ! </h3>
 
-		<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=34">
+		<a href="<?php $contact = get_field('lcontact'); echo $contact; ?>">
 			<div id="estimation">	<p> Demande de devis <p> </div>
 		</a>
 
