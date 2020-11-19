f6857c20041dd2db7803b8cc046a035d2b1d90bb
Priet0 <diegoprieto36@outlook.fr>
2020-11-19 14:38:06 +0100

    lien cliquable home

--- index.php
+++ index.php
@@ -19,37 +19,52 @@
 	</section>
 
 		<div id="text_savoir-faire">
-
-			<div id="border1"> <h2> Notre savoir-faire </h2> </div>
-			<div id="paragraph1">
+			<a href=https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27>
+				<div id="border1"> <h2> Notre savoir-faire </h2> </div>
+				<div id="paragraph1">
 					<p> <?php $sf = get_field('sf_texte'); echo $sf; ?> </p>
-			</div>
-
+				</div>
+			</a>
 		</div>
 
 		<div id="logo_savoir-faire">
 				<h3>Bâti ancien</h3>
 				<div id="wrap1">
-					<div class="icon" name="Restauration"><div class="picture1"><img src="<?php echo get_template_directory_uri();?>/images/logo_restauration.svg" alt="Icône restauration"></div>
-						<div class="icon_name">Restauration</div>
+					<div class="icon" name="Restauration"><div class="picture1">
+						<a href=https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27>
+							<img src="<?php echo get_template_directory_uri();?>/images/logo_restauration.svg" alt="Icône restauration"></div>
+							<div class="icon_name">Restauration</div>
+						</a>
 					</div>
-					<div class="icon" name="Maçonnerie"><div class="picture2"><img src="<?php echo get_template_directory_uri();?>/images/logo_maconnerie.svg" alt="Icône terrassement"></div>
-						<div class="icon_name">Maçonnerie</div>
+					<div class="icon" name="Maçonnerie"><div class="picture2">
+						<a href=https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27>
+							<img src="<?php echo get_template_directory_uri();?>/images/logo_maconnerie.svg" alt="Icône terrassement"></div>
+							<div class="icon_name">Maçonnerie</div>
+						</a>
 					</div>
-					<div class="icon" name="Reprise en sous oeuvre"><div class="picture3"><img src="<?php echo get_template_directory_uri();?>/images/logo_reprise.svg" alt="Icône reprise en sous-oeuvre"></div>
-						<div class="icon_name">Reprise en sous oeuvre</div>
+					<div class="icon" name="Reprise en sous oeuvre"><div class="picture3">
+						<a href=https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27>
+							<img src="<?php echo get_template_directory_uri();?>/images/logo_reprise.svg" alt="Icône reprise en sous-oeuvre"></div>
+							<div class="icon_name">Reprise en sous oeuvre</div>
+						</a>
 					</div>
 				</div>
 				<h3>Maçonnerie traditionnelle</h3>
 				<div id="wrap2">
-					<div class="icon" name="Agrandissement"><div class="picture4"><img src="<?php echo get_template_directory_uri();?>/images/logo_agrandissement.svg" alt="Icône agrandissement"></div>
-						<div class="icon_name">Agrandissement</div>
+					<div class="icon" name="Agrandissement"><div class="picture4">
+						<a href=https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27>
+							<img src="<?php echo get_template_directory_uri();?>/images/logo_agrandissement.svg" alt="Icône agrandissement"></div>
+							<div class="icon_name">Agrandissement</div>
+						</a>
 					</div>
 					<div class="icon" name="Maçonnerie paysagère"><div class="picture5"><img src="<?php echo get_template_directory_uri();?>/images/logo_paysagere.svg" alt="Icône maçonnerie paysagère"></div>
 						<div class="icon_name">Maçonnerie paysagère</div>
 					</div>
-					<div class="icon" name="Enduit"><div class="picture6"><img src="<?php echo get_template_directory_uri();?>/images/logo_enduit.svg" alt="Icône enduit"></div>
-						<div class="icon_name">Enduit</div>
+					<div class="icon" name="Enduit"><div class="picture6">
+						<a href=https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=27>
+							<img src="<?php echo get_template_directory_uri();?>/images/logo_enduit.svg" alt="Icône enduit"></div>
+							<div class="icon_name">Enduit</div>
+						</a>
 					</div>
 				</div>
 
@@ -69,13 +84,16 @@
 			</div>
 
 			<div id="a-propos_text">
+					<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=30">
 						<div id="border2">	<h2> À-propos </h2> </div>
+					</a>
 
 						<div id="paragraph2">
 								<p> <?php $ap = get_field('ap_texte'); echo $ap; ?> </p>
 						</div>
 
-						<p id="more">En savoir plus</p>
+						<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=30"><p id="more">En savoir plus</p></a>
+
 						<div class="absolute"><img src="<?php echo get_template_directory_uri();?>/images/arrow.svg" alt="Aller voir"></div>
 
 			</div>
@@ -87,7 +105,9 @@
 			<h3> Besoin de nos services ?
 				<br/> Contactez-nous ! </h3>
 
-		<div id="estimation">	<p> Demande de devis <p> </div>
+		<a href="https://mmi.univ-smb.fr/~lombarst/siteclient/?page_id=34">
+			<div id="estimation">	<p> Demande de devis <p> </div>
+		</a>
 
 		</div>
 
