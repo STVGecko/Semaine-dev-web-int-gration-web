a733721aeaf6b92c5c0acc3b8a97f7f49b5dabbb
STVGecko <steeven.lombardi@gmail.com>
2020-11-19 18:49:42 +0100

    test

--- page-savoirfaire.php
+++ page-savoirfaire.php
@@ -71,7 +71,6 @@
         <div class="carousel-cell">
           <?php
           $image1 = get_field('image1');
-          var_dump($image1);
           if( !empty( $image1 ) ): ?>
           <img class="carousel-cell-image" src="<?php echo esc_url($image1['url']); ?>" alt="<?php echo esc_attr($image1['alt']); ?>" />
           <?php endif; ?>
