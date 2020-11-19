ba18d12ea79ebafbd85b7434948bbb4c7d8aa173
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-11-19 18:47:22 +0100

    color

--- page-contact.php
+++ page-contact.php
@@ -81,7 +81,7 @@ if(isset($_POST['Envoyer'])) {
   </div>-->
   <div id="devis_contact">
     <h2 id="devis_title">Demande de devis</h2>
-    <p style="display: flex; justify-content: center; color: white; font-size: 18px; font-family: 'Mukta Mahee', sans-serif; background-color: #2c2c2c; width: 50%; text-align: center; margin: 0 auto;">
+    <p style="display: flex; justify-content: center; color: white; font-size: 18px; font-family: 'Mukta Mahee', sans-serif; background-color: #757573; width: 50%; text-align: center; margin: 0 auto;">
       <?php if(isset($msg)) {
            echo $msg;
         }
