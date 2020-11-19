e55885fd59c724d5d9f328fdcabb19b2595c9c0c
Priet0 <diegoprieto36@outlook.fr>
2020-11-19 10:01:06 +0100

    suppression champ, ajout bon champ acf

--- page-apropos.php
+++ page-apropos.php
@@ -2,29 +2,29 @@
 
 <body>
 
-  <h1 class="h1_flex"><?php $titre = get_field('titre_ap'); echo $titre; ?></h1>
+  <h1 class="h1_flex"><?php $titre = get_field('titre'); echo $titre; ?></h1>
   <div id="underline_flex"></div>
   <br/>
   <div id="presentation">
-    <p id="intro_text"><?php $intro = get_field('intro_ap'); echo $intro; ?></p>
+    <p id="intro_text"><?php $intro = get_field('intro'); echo $intro; ?></p>
   </div>
 
   <section class="wrap">
     <div class="bati_ancien">
       <div class="image_gauche">
         <?php
-        $image1_ap = get_field('image1_ap');
-        if( !empty( $image1_ap ) ): ?>
-        <img class="carousel-cell-image" src="<?php echo esc_url($image1_ap['url']); ?>" alt="<?php echo esc_attr($image1_ap['alt']); ?>" />
+        $image1 = get_field('image1');
+        if( !empty( $image1 ) ): ?>
+        <img class="carousel-cell-image" src="<?php echo esc_url($image1['url']); ?>" alt="<?php echo esc_attr($image1['alt']); ?>" />
         <?php endif; ?>
       </div>
     </div>
 
     <div class="texte">
       <img class="cadre_gauche" src="<?php echo get_template_directory_uri();?>/images/cadre_gauche.svg">
-      <h2 class="h2_flex"><?php $titre1 = get_field('titre1_ap'); echo $titre1; ?></h2>
-      <h3 class="h3_flex"><?php $stitre1 = get_field('stitre1_ap'); echo $stitre1; ?></h3> <!--sous-titre ex : gérant de la maçonnerie-->
-      <p class="para"><?php $texte1 = get_field('text1_ap'); echo $texte1; ?></p>
+      <h2 class="h2_flex"><?php $titre1 = get_field('titre1'); echo $titre1; ?></h2>
+      <h3 class="h3_flex"><?php $stitre1 = get_field('stitre1'); echo $stitre1; ?></h3> <!--sous-titre ex : gérant de la maçonnerie-->
+      <p class="para"><?php $texte1 = get_field('text1'); echo $texte1; ?></p>
   </div>
 </div>
 
@@ -32,9 +32,9 @@
 <div class="maçonnerie">
   <div class="image_droit">
   <?php
-//  $image2_ap = get_field('image2_ap');
-  //if( !empty( $image2_ap ) ): ?>
-  <img class="carousel-cell-image" src="<?php //echo esc_url($image2_ap['url']); ?>" alt="<?php //echo esc_attr($image2_ap['alt']); ?>" />
+//  $image2 = get_field('image2');
+  //if( !empty( $image2 ) ): ?>
+  <img class="carousel-cell-image" src="<?php //echo esc_url($image2['url']); ?>" alt="<?php //echo esc_attr($image2['alt']); ?>" />
   <?php //endif; ?>
   </div>
 
--- page-savoirfaire.php
+++ page-savoirfaire.php
@@ -2,11 +2,11 @@
 
 <body>
 
-  <h1 class="h1_flex"><?php $titre = get_field('titre_sf'); echo $titre; ?></h1>
+  <h1 class="h1_flex"><?php $titre = get_field('titre'); echo $titre; ?></h1>
   <div id="underline_flex"></div>
   <br/>
   <div id="presentation">
-    <p id="intro_text"><?php $intro = get_field('intro_sf'); echo $intro; ?></p>
+    <p id="intro_text"><?php $intro = get_field('intro'); echo $intro; ?></p>
   </div>
 
   <section class="wrap">
@@ -18,7 +18,7 @@
     <div class="carousel-cell">
 
     <?php
-    $image1 = get_field('image1_sf');
+    $image1 = get_field('image1');
     if( !empty( $image1 ) ): ?>
     <img class="carousel-cell-image" src="<?php echo esc_url($image1['url']); ?>" alt="<?php echo esc_attr($image1['alt']); ?>" />
     <?php endif; ?>
@@ -26,7 +26,7 @@
     </div>
     <div class="carousel-cell">
       <?php
-      $image1 = get_field('image1_sf');
+      $image1 = get_field('image1');
       if( !empty( $image1 ) ): ?>
       <img class="carousel-cell-image" src="<?php echo esc_url($image1['url']); ?>" alt="<?php echo esc_attr($image1['alt']); ?>" />
       <?php endif; ?>
@@ -34,7 +34,7 @@
     </div>
     <div class="carousel-cell">
       <?php
-      $image1 = get_field('image1_sf');
+      $image1 = get_field('image1');
       if( !empty( $image1 ) ): ?>
       <img class="carousel-cell-image" src="<?php echo esc_url($image1['url']); ?>" alt="<?php echo esc_attr($image1['alt']); ?>" />
       <?php endif; ?>
@@ -49,9 +49,9 @@
 
     <div class="texte">
       <img class="cadre_gauche" src="<?php echo get_template_directory_uri();?>/images/cadre_gauche.svg">
-      <h2 class="h2_flex"><?php $titre1 = get_field('titre1_sf'); echo $titre1; ?></h2>
-      <h3 class="h3_flex"><?php $stitre1 = get_field('stitre1_sf'); echo $stitre1; ?></h3> <!--sous-titre ex : gérant de la maçonnerie-->
-      <p class="para"><?php $texte1 = get_field('text1_sf'); echo $texte1; ?></p>
+      <h2 class="h2_flex"><?php $titre1 = get_field('titre1'); echo $titre1; ?></h2>
+      <h3 class="h3_flex"><?php $stitre1 = get_field('stitre1'); echo $stitre1; ?></h3> <!--sous-titre ex : gérant de la maçonnerie-->
+      <p class="para"><?php $texte1 = get_field('text1'); echo $texte1; ?></p>
   </div>
 </div>
 
@@ -62,9 +62,9 @@
   </div>
 
   <div class="texte">
-    <h2 class="h2_flex"><?php //$titre2 = get_field('titre2_sf'); echo $titre2; ?></h2>
-    <h3 class="h3_flex"><?php// $stitre2 = get_field('stitre2_sf'); echo $stitre2; ?></h3> sous-titre ex : gérant de la maçonnerie
-    <p class="para"><?php //$texte2 = get_field('text2_sf'); echo $texte2; ?></p>
+    <h2 class="h2_flex"><?php //$titre2 = get_field('titre2'); echo $titre2; ?></h2>
+    <h3 class="h3_flex"><?php// $stitre2 = get_field('stitre2'); echo $stitre2; ?></h3> sous-titre ex : gérant de la maçonnerie
+    <p class="para"><?php //$texte2 = get_field('text2'); echo $texte2; ?></p>
 </div>
 </div>
 </section>
