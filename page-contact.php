3042d050888e585011c0f607578de214e63ab03b
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-11-19 11:34:23 +0100

    input

--- page-contact.php
+++ page-contact.php
@@ -62,7 +62,7 @@
         <div class="contact_message"><p class="contact_names" type="Message"><input type="textarea" class="contact_input" placeholder="Ecrivez votre message ici" name="message"></p><p class="en_savoir_plus">En envoyant ce message, vous consentez à la collecte et au traitement des données renseignées ci-dessus pour l’usage exclusif de
 M2A Maçonnerie.<a href="https://fr.eni.com/particuliers/cookies"> En savoir plus </a>></p></div>
         <div id="contact_button">
-          <input class="contact_button" type="submit">Envoyer</button>
+          <input class="contact_button" type="submit">Envoyer</input>
         </div>
       </div>
     </form>
