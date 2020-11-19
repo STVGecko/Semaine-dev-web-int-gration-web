47985c1e3174759a5ff5b4a932799da0de220c9f
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-11-19 18:23:17 +0100

    modif tailles

--- page-contact.php
+++ page-contact.php
@@ -81,7 +81,7 @@ if(isset($_POST['Envoyer'])) {
   </div>-->
   <div id="devis_contact">
     <h2 id="devis_title">Demande de devis</h2>
-    <p style="display: flex; justify-content: center; color: white; font-size: 18px; font-family: 'Mukta Mahee', sans-serif; background-color: #2c2c2c; width: 50%">
+    <p style="display: flex; justify-content: center; color: white; font-size: 18px; font-family: 'Mukta Mahee', sans-serif; background-color: #2c2c2c; width: 50%; text-align: center;">
       <?php if(isset($msg)) {
            echo $msg;
         }
