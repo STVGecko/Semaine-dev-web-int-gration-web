a8c03eca5a282d0b745721003e034e0590c181aa
STVGecko <steeven.lombardi@gmail.com>
2020-11-20 09:36:11 +0100

    finis stp

--- page-savoirfaire.php
+++ page-savoirfaire.php
@@ -11,7 +11,7 @@
 
 
     <div class="oldb">
-  <div class="carrousel_sf_haut">
+  <div class="carrousel_sf_h">
       <!-- Flickity HTML init -->
       <div class="carousel"
       data-flickity='{ "lazyLoad": true }'>
