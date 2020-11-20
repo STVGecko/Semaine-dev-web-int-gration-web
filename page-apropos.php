f48ac8e60842fea16cfeb6aaeae4cc7f145c4183
Priet0 <diegoprieto36@outlook.fr>
2020-11-20 12:02:12 +0100

    test css flexible

--- page-apropos.php
+++ page-apropos.php
@@ -20,13 +20,12 @@
       </div>
 
       <div class="frame_assembly_h">
-        <img class="cadre_l" src="<?php echo get_template_directory_uri();?>/images/cadre_gauche.svg">
           <div class="text">
             <h2 class="h2_flex"><?php $titre1 = get_field('titre1'); echo $titre1; ?></h2>
             <h3 class="h3_flex"><?php $stitre1 = get_field('stitre1'); echo $stitre1; ?></h3> <!--sous-titre ex : gérant de la maçonnerie-->
             <p class="para"><?php $texte1 = get_field('text1'); echo $texte1; ?></p>
+            <img class="cadre_l" src="<?php echo get_template_directory_uri();?>/images/cadre_gauche.svg">
           </div>
-        </img> 
       </div>
   </div>
 </div>
