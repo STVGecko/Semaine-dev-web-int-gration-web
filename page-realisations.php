d170da25ba3ab2c5e3df3b02674c000190c77639
AnthonyNhlg <antho.nehlig@gmail.com>
2020-11-20 13:45:29 +0100

    foreach realisation

--- page-realisations.php
+++ page-realisations.php
@@ -3,6 +3,8 @@
 <body>
   <section id="realization_page">
 
+    <?php foreach($realisations as $line): ?>
+
     <div class="zoom wrap_realization">
       <h2 id="realization_title"><?php $titre_rea = get_field('titre_rea'); echo $titre_rea; ?></h2>
       <!--<p class="para">Description possible du projet<?php /*$texte1 = get_field('text1'); echo $texte1;*/ ?></p>-->
@@ -19,6 +21,8 @@
       </div>
 
     </div>
+
+  <?php endforeach; ?>
 
   </section>
 
