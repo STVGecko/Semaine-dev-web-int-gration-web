9eeb3f86a8bed275ffa49a04189562514f58738b
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-11-20 10:19:34 +0100

    smooth scroll

--- index.php
+++ index.php
@@ -120,22 +120,5 @@
 
 </div>
 
-<script type="text/javascript">/*anim scroll*/
-$(function() {
-  
-    $(".scroll[href*='#']:not([href='#'])").click(function() {
-        if (
-            location.hostname == this.hostname
-            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
-        ) {
-            var anchor = $(this.hash);
-            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
-            if ( anchor.length ) {
-                $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
-            }
-        }
-    });
-});
 
-</script>
 <?php get_footer(); ?>
