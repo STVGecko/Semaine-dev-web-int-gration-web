e4c9c8f102ba86e63af2757d1fb09ace9da915d0
STVGecko <steeven.lombardi@gmail.com>
2020-11-23 15:40:00 +0100

    test

--- page-realisations.php
+++ page-realisations.php
@@ -3,7 +3,7 @@
 
   <section class="wrap_realization">
     <?php $projet = get_field('project'); ?>
-    <?php foreach ($projet as $project): ?>
+    <?phpif(is_array($projet)) foreach ($projet as $project): ?>
       <h1 class="title_realization"><?php echo($project['title_realization']); ?></h1>
       <div class="underline_realization"></div>
       <div class="image_set_realization">
