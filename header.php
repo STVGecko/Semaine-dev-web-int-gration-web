2f882f369b4dd3bc315972ace5d80a770157d8b6
STVGecko <steeven.lombardi@gmail.com>
2020-11-23 15:59:39 +0100

    header.php jquery

--- header.php
+++ header.php
@@ -6,8 +6,6 @@
 	<meta http-equiv="cache-control" content="no-cache"/>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-	<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 	<?php wp_head(); ?>
 </head>
