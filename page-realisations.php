d4306efe616be824a98d565aca12b70d15e7feaa
STVGecko <steeven.lombardi@gmail.com>
2020-11-23 16:39:23 +0100

    realisation

--- page-realisations.php
+++ page-realisations.php
@@ -1,8 +1,9 @@
 <?php get_header(); ?>
 
+  <section class="wrap_realization">
     <?php $projet = get_field('project'); ?>
     <?php foreach ($projet as $project): ?>
-  <section class="wrap_realization">
+
       <h1 class="title_realization"><?php echo($project['title_realization']); ?></h1>
       <div class="underline_realization"></div>
       <div class="image_set_realization">
@@ -17,8 +18,8 @@
           <img src="<?php echo($project['picture_project_realization6']['url']); ?>" alt="image6">
         </div>
       </div>
-  </section>
     <?php endforeach; ?>
+  </section>
 </body>
 
 <?php get_footer(); ?>
