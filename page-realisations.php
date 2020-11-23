c32cffb8597d81d5c97f1510f3ab484185e2b4f4
STVGecko <steeven.lombardi@gmail.com>
2020-11-23 15:53:07 +0100

    c bon ça apparait

--- page-realisations.php
+++ page-realisations.php
@@ -7,12 +7,12 @@
       <h1 class="title_realization"><?php echo($project['title_realization']); ?></h1>
       <div class="underline_realization"></div>
       <div class="image_set_realization">
-        <div class="image_set1">
+        <div class="image_set">
           <img src="<?php echo($project['picture_project_realization1']['url']); ?>" alt="image1">
           <img src="<?php echo($project['picture_project_realization2']['url']); ?>" alt="image2">
           <img src="<?php echo($project['picture_project_realization3']['url']); ?>" alt="image3">
         </div>
-        <div class="image_set2">
+        <div class="image_set">
           <img src="<?php echo($project['picture_project_realization4']['url']); ?>" alt="image4">
           <img src="<?php echo($project['picture_project_realization5']['url']); ?>" alt="image5">
           <img src="<?php echo($project['picture_project_realization6']['url']); ?>" alt="image6">
