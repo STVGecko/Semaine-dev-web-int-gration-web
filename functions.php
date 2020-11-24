72b1c6a4aaa8d4c5d87b8c6749f4cea3ed865a34
STVGecko <steeven.lombardi@gmail.com>
2020-11-24 10:07:11 +0100

    realisation

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ function scripts() {
   wp_enqueue_style( 'footer', get_template_directory_uri().'/footer/css/footer.css');
   wp_enqueue_style( 'footer.responsive', get_template_directory_uri().'/footer/css/footer.responsive.css');
   wp_enqueue_style( 'page-fexible', get_template_directory_uri().'/aproposetsavoirfaire/css/page-flexible.css');
-  wp_enqueue_style( 'page-realisations', get_template_directory_uri().'/css/page-realisations.css');
+  wp_enqueue_style( 'page-realisations', get_template_directory_uri().'/realisation/css/page-realisations.css');
   wp_enqueue_style( 'page-contact', get_template_directory_uri().'/contact/css/page-contact.css');
   //wp_enqueue_script(); si besoin de js
 }
