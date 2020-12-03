9a162dc1df67809ca0ebd1b3abaa0c5743bafca2
STVGecko <steeven.lombardi@gmail.com>
2020-12-03 09:24:24 +0100

    die en poi

--- page-contact.php
+++ page-contact.php
@@ -34,18 +34,18 @@ if(isset($_POST['Envoyer'])) {
 
 <body>
   <!-- Présentation de la page -->
-<section id="contact_page" style="margin-top: 150px;
+<section class="contact_page" style="margin-top: 150px;
     margin-bottom: 145px;margin-left:5%;">
-  <h2 id="contact_title">Pour nous contacter</h2>
-  <div id="underline_contact"></div>
+  <h2 class="contact_title">Pour nous contacter</h2>
+  <div class="underline_contact"></div>
   <br>
-  <div id="presentation">
-    <p id="bold_text">Pierre-André FLAMENS, gérant de la société M2A</p>
-    <p id="small_text">Si vous avez la moindre question, ou si vous souhaitez nous demander un devis, n’hésitez pas.</p>
+  <div class="presentation">
+    <p class="bold_text">Pierre-André FLAMENS, gérant de la société M2A</p>
+    <p class="small_text">Si vous avez la moindre question, ou si vous souhaitez nous demander un devis, n’hésitez pas.</p>
     <div class="test"></div><!-- rajouter num avec wordpress-->
   </div>
   <!-- Partie image -->
-  <div id="wrap_contact">
+  <div class="wrap_contact">
     <div class="icon_contact" name="Telephone"  onclick="location.href='tel:+`<?php $tel = get_field('telephone', get_option('page_on_front')); echo $tel; ?>`'">
       <img src="<?php echo get_template_directory_uri();?>/images/phone.svg" alt="Icône téléphone">
       <div class="text_contact" name="Telephone">
@@ -68,7 +68,7 @@ if(isset($_POST['Envoyer'])) {
     </div>
   </div>
     <!-- Partie texte -->
-  <!--<div id="text_wrap_contact">
+  <!--<div class="text_wrap_contact">
     <div class="text_contact" name="Telephone">
       <p>06 74 11 63 45</p>
     </div>
@@ -80,8 +80,8 @@ if(isset($_POST['Envoyer'])) {
       <p>Ouvrir dans Maps ></p>
     </div>
   </div>-->
-  <div id="devis_contact">
-    <h2 id="devis_title">Demande de devis</h2>
+  <div class="devis_contact">
+    <h2 class="devis_title">Demande de devis</h2>
     <p style="display: flex; justify-content: center; color: black; font-size: 14px; font-family: 'Mukta Mahee', sans-serif; width: 50%; text-align: center; margin: 0 auto; padding-top: 10px;">
       <?php if(isset($msg)) {
            echo $msg;
@@ -89,20 +89,20 @@ if(isset($_POST['Envoyer'])) {
         ?>
     </p>
     <form class="form" action="<?php the_permalink() ?>" method="POST"><!-- DEBUT FORMULAIRE-->
-      <div id="englobe_all">
-        <div id="contact_gauche">
+      <div class="englobe_all">
+        <div class="contact_gauche">
           <p class="contact_names" type="Prénom"><input class="contact_input" placeholder="Ecrivez votre prénom ici..." name="prenom" type="text"></p>
           <p class="contact_names" type="Mail"><input class="contact_input" placeholder="Ecrivez votre mail ici..." name="mail" type="email" value="<?php if(isset($_POST['mail'])) { echo $_POST['mail']; } ?>"></p>
         </div><!-- FIN CONTACT GAUCHE-->
 
-        <div id="contact_droite">
+        <div class="contact_droite">
           <p class="contact_names" type="Nom"> <input class="contact_input" placeholder="Ecrivez votre nom ici..." name="nom" value="<?php if(isset($_POST['nom'])) { echo $_POST['nom']; } ?>" ></input></p>
           <p class="contact_names" type="Téléphone"> <input class="contact_input" placeholder="Ecrivez votre téléphone ici..." name="telephone"></input></p>
         </div><!-- FIN CONTACT DROITE-->
         <div class="contact_message"><p class="contact_names" type="Message"><input type="textarea" class="contact_input" placeholder="Ecrivez votre message ici" name="message" value="<?php if(isset($_POST['message'])) { echo $_POST['message']; } ?>"></p><p class="en_savoir_plus">En envoyant ce message, vous consentez à la collecte et au traitement des données renseignées ci-dessus pour l’usage exclusif de
 M2A Maçonnerie.</p><a href="https://www.cnil.fr/fr/rgpd-exemples-de-mentions-dinformation
 " target="_blank" class="lien_en_savoir_plus"> En savoir plus ></a></div>
-        <div id="contact_button">
+        <div class="contact_button">
           <input class="contact_button" type="submit" value="Envoyer" name="Envoyer"></input>
         </div>
       </div>
