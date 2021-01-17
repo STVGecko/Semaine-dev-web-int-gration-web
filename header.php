6ced4abdff26855c420073793d024103e468fc28
STVGecko <steeven.lombardi@gmail.com>
2021-01-17 18:37:15 +0100

    test

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 	<meta http-equiv="cache-control" content="no-cache"/>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-	<script src="header/script.js"></script>
+	<script type= "text/javascript" src="header/script.js"></script>
 	<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
