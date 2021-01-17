8bed3f69e5eee781bc668c9618f786773d7e3484
STVGecko <steeven.lombardi@gmail.com>
2021-01-17 19:33:25 +0100

    test

--- header.php
+++ header.php
@@ -10,8 +10,8 @@
 	const hamburger = document.getElementById('hamburger');
 	const navUL = document.getElementById('nav');
 
-	hamburger.addEventListener('click', () =>){
-		navUL.classList.toggle('show');
+	hamburger.addEventListener('click', fonction(e) =>){
+		e.navUL.classList.toggle('show');
 	});
 
 	console.log("je suis là !!!!!!!!!!!!!!!");
