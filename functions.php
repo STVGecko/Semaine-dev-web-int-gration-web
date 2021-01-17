e77d47f1616576cddf3670e15021ba7247ea6ea6
STVGecko <steeven.lombardi@gmail.com>
2021-01-17 19:14:11 +0100

    test

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 //ajout de css et de js dans le header
 function scripts() {
-  wp_enqueue_script( 'scripts', get_template_directory_uri().'/header/scripts.js');
+  wp_enqueue_script( 'scripts', get_template_directory_uri().'/header/js/scripts.js');
   wp_enqueue_style( 'reset', get_template_directory_uri().'/css/reset.css');
   wp_enqueue_style( 'index', get_template_directory_uri().'/css/index.css');
   wp_enqueue_style( 'header', get_template_directory_uri().'/header/css/header.css');
