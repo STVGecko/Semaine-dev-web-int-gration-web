44be957f139e0f47f51462928e9d16a8f9989d17
STVGecko <steeven.lombardi@gmail.com>
2021-01-19 13:31:32 +0100

    s

--- page-contact.php
+++ page-contact.php
@@ -96,10 +96,6 @@ M2A Maçonnerie.</p><a href="https://www.cnil.fr/fr/rgpd-exemples-de-mentions-di
     <!-- FIN FORMULAIRE -->
   </div>
 </section>
-
-<?php
- $yo=get_field('reception_mail'); echo $yo; ?>
-
 </body>
 
 <?php get_footer(); ?>
