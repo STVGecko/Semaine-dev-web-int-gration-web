a0280627d1f36226ef196c3c67248e9ed95234fb
STVGecko <steeven.lombardi@gmail.com>
2021-01-21 10:19:13 +0100

    dq

--- index.php
+++ index.php
@@ -92,7 +92,11 @@
 		<div class="contact">
 			<h3> Besoin de nos services ?
 				<br/> Contactez-nous ! </h3>
-			<div class="estimation_index"><a href="<?php $contact = get_field('lcontact'); echo $contact; ?>">	<p> Demande de devis <p> </a></div>
+			<div class="estimation_index">
+				<a href="<?php $contact = get_field('lcontact'); echo $contact; ?>">
+					<p> Demande de devis <p>
+				</a>
+			</div>
 		</div>
 
 </div>
