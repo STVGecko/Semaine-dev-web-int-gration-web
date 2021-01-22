9aabb308a91268c98bc72c7f38131697340e6652
STVGecko <steeven.lombardi@gmail.com>
2021-01-22 15:41:09 +0100

    zqdzq

--- header/js/scripts.js
+++ header/js/scripts.js
@@ -2,5 +2,5 @@ var mem = $('.text_icon').text();
     mem = mem.replace('arobaze','@');
 var mem2 = $('p.mail-replace').text();
     mem2 = mem2.replace('arobaze','@');
-    $('.text_icon_replace').html(mem);
+    $('.text_icon-replace').html(mem);
     $('p.mail-replace').html(mem2);
