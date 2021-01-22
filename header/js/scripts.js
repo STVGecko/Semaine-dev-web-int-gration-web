67de8cdd386ea3d1c09319f9d56dbaf11c7355c3
STVGecko <steeven.lombardi@gmail.com>
2021-01-22 15:32:48 +0100

    correct ?

--- header/js/scripts.js
+++ header/js/scripts.js
@@ -1,4 +1,6 @@
 var mem = $('.text_icon').text();
     mem = mem.replace('arobaze','@');
+var mem2 = $('p.fine_font').text();
+    mem2 = mem2.replace('arobaze','@');
     $('.text_icon').html(mem);
     $('p.fine_font').html(mem);
