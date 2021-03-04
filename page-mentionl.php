2b39e65f36a2a720f1895bc1fe3a21f298ceb6f1
STVGecko <steeven.lombardi@gmail.com>
2021-03-04 18:23:57 +0100

    mentions légales finies

--- page-mentionl.php
+++ page-mentionl.php
@@ -10,12 +10,12 @@ Template Name: Mentions légales
 
     <h1 class="h1_flex">Mentions légales</h1>
     <div class="underline_realization"></div>
-    <p class="title_mentionl">Intégrateurs web</p>
+    <p class="title_mentionl">Intégrateurs web :</p>
     <p class="info_mentionl">Steeven Lombardi, Clément Desouches Lucas Reymonet, Diego Prieto-Rodriguez</p>
-    <p class="title_mentionl">Graphistes</p>
+    <p class="title_mentionl">Graphistes :</p>
     <p class="info_mentionl">Loriane Kah, Anthony Nehlig, Côme Fisset</p>
     <p class="info_mentionl">Agence Kanzaï ©2020-2021</p>
-
+    
     <?php $mentions_legales = get_field('mentions_legales');?>
     <?php //var_dump($mentions_legales);?>
     <?php foreach ($mentions_legales as $mention_legale): ?>
