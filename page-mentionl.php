23ba55b9d999e321715e809a969be4fb17351cbb
STVGecko <steeven.lombardi@gmail.com>
2021-03-04 16:20:56 +0100

    mentionl

--- page-mentionl.php
+++ page-mentionl.php
@@ -8,10 +8,8 @@
 
     <?php $mentions_legales = get_field('mentions_legales'); ?>
     <?php foreach ($mentions_legales as $mention_legale): ?>
-
     <div class="title_mentionl"><?php $propos = get_field('titre_mentions_legales'); echo $propos; ?></div>
     <div class="info_mentionl"><?php $propos = get_field('infos_mentions_legales'); echo $propos; ?></div>
-
     <?php endforeach; ?>
   </section>
 </body>
