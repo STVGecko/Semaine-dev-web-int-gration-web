e58896d3b92612b1a8edb705a1e7a25d6114192b
STVGecko <steeven.lombardi@gmail.com>
2021-03-04 16:47:26 +0100

    réglage

--- page-mentionl.php
+++ page-mentionl.php
@@ -3,13 +3,13 @@
   <section class="wrap_realization">
 
     <h1 class="h1_flex">Mentions légales</h1>
-    <div class="title_mentionl_kanzai"></div>
-    <div class="info_mentionl_kanzai"></div>
+    <p class="title_mentionl_kanzai"></p>
+    <p class="info_mentionl_kanzai"></p>
 
     <?php $mentions_legales = get_field('mentions_legales'); ?>
     <?php foreach ($mentions_legales as $mention_legale): ?>
-    <div class="title_mentionl"><?php $propos = get_field('titre_mentions_legales'); echo $propos; ?></div>
-    <div class="info_mentionl"><?php $propos = get_field('infos_mentions_legales'); echo $propos; ?></div>
+    <p class="title_mentionl"><?php $propos = get_field('titre_mentions_legales'); echo $propos; ?></p>
+    <p class="info_mentionl"><?php $propos = get_field('infos_mentions_legales'); echo $propos; ?></p>
     <?php endforeach; ?>
   </section>
 </body>
